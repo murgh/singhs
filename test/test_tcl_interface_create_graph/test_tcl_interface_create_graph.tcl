44ff903d97da37db68cd0170a3fb88436aaece4a
Beenish <user@UPLC.(none)>
2015-11-08 00:32:59 +0530

    Adding test case

---
+++ test/test_tcl_interface_create_graph/test_tcl_interface_create_graph.tcl
@@ -0,0 +1,11 @@
+load /home/user/MP/bee/mymodule.so
+
+
+create_graph -name graph1 -type directional
+
+create_graph -type bidirectional -name graph2
+
+create_graph -nam graph3 -type undirectional
+
+create_graph -name graph4 -typ directional
+
